03b321e6d68b3d0ae661a11b9e5818e6a7c2f09c
nekonako <code.yuune@gmail.com>
2020-12-25 12:09:38 +0700

    nganu: fixing pwa

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,10 +13,6 @@
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/precache-manifest.1e53d7387c17ec6da0fee377157c86ce.js"
-);
-
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
